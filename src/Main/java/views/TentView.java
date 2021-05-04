0eb1f162b1ee29bb59a3b11765ae00a375ce82fd
Tassi Timian <timian.tassi@gmail.com>
2021-05-04 22:32:14 +0200

    Gradle configuration

---
+++ src/Main/java/views/TentView.java
@@ -0,0 +1,45 @@
+package views;
+
+import Field.Field;
+import Item.Tent;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * sator item nezet
+ */
+public class TentView implements IView {
+    Tent tent;
+    BufferedImage image;
+    BufferedImage imageO;
+    public TentView(Tent t){
+        tent = t;
+        try{
+            image = (BufferedImage) ImageIO.read(new File("src/main/resources/tent.png"));
+            imageO = (BufferedImage) ImageIO.read(new File("src/main/resources/tent_fifty.png"));
+        }
+        catch(IOException e){
+            System.out.println("Valami baj van a sátor i/o-val");
+        }
+    }
+
+    /**
+     * sator item rajzolas
+     * @param graphics
+     */
+    @Override
+    public void Draw(Graphics graphics) {
+        if (tent.getHolder() == null) {
+            Field field = tent.getField();
+            if(field.getLayerOfSnow()==0){
+                if(field.IsOpen())graphics.drawImage(image, field.X*64, field.Y*64, null );
+                else graphics.drawImage(imageO, field.X*64, field.Y*64, null );
+            }
+        }
+    }
+
+}
