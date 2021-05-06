4e572ffad0bfa2a888f20e7c763753ee50f4de15
Tassi Timian <timian.tassi@gmail.com>
2021-05-06 20:56:39 +0200

    Test for CI

---
+++ src/test/java/IglueTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class IglueTest {
+
+    @Test
+    public void CI_Test(){
+
+        assertTrue(true);
+    }
+}
